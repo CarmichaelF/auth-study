8e0175dc763e8d7a6c8942afbbaa173b701e7fdd
Carmichael00 <cpereira.extpar@Commonwealth.com>
2022-04-13 10:11:51 -0300

    cleaning the template code and adding chakra

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
-import { Header } from '../public/components/Header'
+import { Input, Stack } from '@chakra-ui/react';
+import { useState } from 'react';
 
 export default function Home() {
-  return <Header></Header>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Stack spacing="6" align="center" justify="center" height="100vh" maxW="500px" margin="0 auto">
+      <Input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input
+        type="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+    </Stack>
+  );
 }
